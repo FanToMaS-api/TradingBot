446b2c13e134cbbaba82a06cf530021c243cac87
FanToMaS-api <misha.dulov@mail.ru>
2022-02-10 11:30:48 +0300

    TB-3 Добавил основные классы и интерфейсы

---
+++ ExchangeLibrary/src/Binance/WebSoket/IBinanceWebSocketService.cs
@@ -0,0 +1,10 @@
+﻿namespace ExchangeLibrary.Binance.WebSoket
+{
+    /// <summary>
+    ///     Сервис управления подписками на стримы данных с бинанса 
+    /// </summary>
+    public interface IBinanceWebSocketService
+    {
+
+    }
+}
---
+++ ExchangeLibrary/src/Binance/WebSoket/Impl/BinanceWebSocketWrapper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExchangeLibrary.Binance.WebSoket.Impl
+{
+    /// <summary>
+    ///     Оболочка над Binance websocket
+    /// </summary>
+    internal class BinanceWebSocketWrapper
+    {
+        #region Fields
+
+        private readonly IBinanceWebSocketHumble _webSocketHumble;
+        private List<Func<string, Task>> _onMessageReceivedFunctions = new();
+        private List<CancellationTokenRegistration> _onMessageReceivedCancellationTokenRegistrations = new();
+        private CancellationTokenSource _loopCancellationTokenSource;
+        private Uri _url;
+        private int _receiveBufferSize;
+
+        #endregion
+
+        #region .ctor
+
+        /// <inheritdoc cref="BinanceWebSocketWrapper"/>>
+        public BinanceWebSocketWrapper(IBinanceWebSocketHumble socketHumble, string url, int receiveBufferSize = 8192)
+        {
+            _webSocketHumble = socketHumble;
+            _url = new Uri(url);
+            _receiveBufferSize = receiveBufferSize;
+        }
+
+        #endregion
+
+        #region Public methods
+
+
+
+        #endregion
+    }
+}
